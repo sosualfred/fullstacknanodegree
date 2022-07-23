51f9d13e9ade37006435d4d6b92769cb70de949d
sosualfred <sosualfred@gmail.com>
2022-07-23 21:12:55 +0000

    add migration

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
-db.create_all()
+# db.create_all()
 
 
 @app.route('/')
